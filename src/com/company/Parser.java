b9afd230846b765dd082a765dfa1bd163593dd72
iamnatali <55502340+iamnatali@users.noreply.github.com>
2019-10-06 19:23:52 +0500

    неполная реализация работы с базой данных

--- src/com/company/Parser.java
+++ src/com/company/Parser.java
@@ -5,18 +5,14 @@ import java.util.Arrays;
 import java.util.List;
 
 class Parser {
-    //private List<String> buttonsNames;
     private String conversation;
-    private PetBot pet;
+    public PetBot pet;
     private final List<String> genders=Arrays.asList("девочка", "мальчик", "трангендер");
 
     //сделать conversation классом
-    //if (conversation.equals("name")) можно сделать выбор животного/породы
     //обработать все возможные ошибки пользователя
     //команды в enum
     //повторения return
-    //уже сделанные разные клавиатуры(?)
-    //создать объект парсера из строки и кнопок и хайда
 
     String GetAudio(String rawstr){
         if (conversation.equals("fullpet") && rawstr.equals("/caress")){
@@ -43,10 +39,10 @@ class Parser {
     String GetParsedString(String rawstr){
         String parsedString = "Не могу вас понять";
         if (rawstr.equals("/help")){
-            parsedString = "Добро пожаловать в чат общения с вашим питоцем./n" +
-                    "Используйте команду /start, чтобы завести питомца/n" +
-                    "А затем заботьтесь о нем командами /caress и /feed" +
-                    "Если вы будете хорошим хозяином, то цвидите как он растет и учится новому)" +
+            parsedString = "Добро пожаловать в чат общения с вашим питоцем.\n" +
+                    "Используйте команду /start, чтобы завести питомца\n" +
+                    "А затем заботьтесь о нем командами /caress и /feed\n" +
+                    "Если вы будете хорошим хозяином, то цвидите как он растет и учится новому)\n" +
                     "Не забывайте о нем, ведь без вас он погибнет!";
             return parsedString;
         }
